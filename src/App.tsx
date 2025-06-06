102b0c8782560810bf97a662ecef5d574aff8b74
Alejandroch <alejandro9.ch321@gmail.com>
2025-06-06 00:07:55 -0500

    chore: configure routes for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
      <Router>
       <Routes>
         <Route path="/" element={<Cuestionario />} />
-        <Route path="/download" element={<Admin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
